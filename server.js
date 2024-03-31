53c5998d9035dddd899788137572586adb8efa67
ImranAlli12 <163504909+ImranAlli12@users.noreply.github.com>
2024-03-31 21:33:00 +0500

    use createProxyMiddleware  for the production

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const connectionDB = require("./src/config/db");
-const userRoutes = require("./src/routes/user.routes")
-const chatRoutes = require("./src/routes/chat.routes")
-const messageRoutes = require("./src/routes/message.routes")
-
+const connectionDB = require("./config/db");
+const userRoutes = require("./routes/user.routes")
+const chatRoutes = require("./routes/chat.routes")
+const messageRoutes = require("./routes/message.routes")
+const { createProxyMiddleware } = require('http-proxy-middleware');
 const cors = require('cors');
 
 const app = express();
@@ -13,14 +13,21 @@ connectionDB();
 app.use(cors());
 
 app.use(express.json());
-
+// **Development (Optional - Comment out for production):**
+if (process.env.NODE_ENV === 'development') {
+    app.use(cors({ origin: "http://localhost:3000", methods: ["GET", "POST"] })); // Enable CORS for local development
+  }
 app.get("/", (req, res) => {
     console.log("welcome to chat app");
 })
-
-app.use("/api/user", userRoutes);
-app.use("/api/chat", chatRoutes);
+const proxy = createProxyMiddleware({
+    target: 'https://seo-chat-server-p5rk.vercel.app/', // Replace with your Vercel domain
+    changeOrigin: true, // Ensures CORS headers are adjusted correctly
+});
+app.use("/api/user", userRoutes );
+app.use("/api/chat", chatRoutes );
 app.use("/api/message", messageRoutes);
+app.use(proxy);
 
 const port =  8001
 const server = app.listen(port, console.log("server is running at post = ", port));
@@ -29,8 +36,11 @@ const io = require("socket.io")(server, {
     cors: {
         origin: "http://localhost:3000",
         methods: ["GET", "POST"]
+
     },
 });
+
+
 
 io.on("connection", (socket) => {
     socket.on("setup", (userData) => {
